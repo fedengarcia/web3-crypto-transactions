7f6a57d636ea9a45f3d5fd201f18e910b5bc3bdb
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-12-07 09:04:50 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Navbar, Home, Footer, Services, Transactions} from './components';
 const  App = () => {
   return (
     <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
+      <div className="gradient-bg-home">
           <Navbar/>
           <Home/>
       </div>
-      
+      <h1 class="text-5x2 font-bold">
+    Hello world!
+  </h1>
       <Services/>
       <Transactions/>
       <Footer/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+import logo from '../img/logo192.png'
+
+const NavbarItem = ({title, classProps}) => {
+
+  return(<li className="navbar_item">
+    {title}
+  </li>)
+}
+
+
 
 const Navbar = () => {
     
     return (
-        <div>
-          <h1> Navbar</h1>  
-        </div>
+        <nav className='navbar'>
+          <div className="banner">
+            <img  src={logo} alt="logo" className='w-32 cursor-pointer'/>
+            <button className='wallet_btn'>
+                Connect Wallet
+            </button>
+          </div>
+          <ul className='navbar_list'>
+          {/* <FontAwesomeIcon size='x1' icon={faEnvelope} /> */}
+              <NavbarItem title={"Market"}/>
+              <NavbarItem title={"Exchange"}/>
+              <NavbarItem title={"Tutorials"}/>
+              <NavbarItem title={"Wallets"}/>
+          </ul>
+        </nav>
     );
 }
 
