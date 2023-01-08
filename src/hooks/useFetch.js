f324020dd032f1fb411e72582c3b98356d2bac92
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-01-08 16:41:34 +0100

    Create useFetch.js
    Get gifphy hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import React, { useEffect,useState } from "react";
+
+const API_KEY =  process.env.GIPHY_API
+
+
+const useFetch = ({keyword}) => {
+    const [gifUrl, setGifUrl] = useState("");
+
+
+    const fetchGifs = async () => {
+        try {
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`)
+            const { data } = await response.json();
+            setGifUrl(data[0]?.images?.downsized_medium?.url)
+        } catch (error) {
+            setGifUrl(`https://api.giphy.com/v1/gifs/search?api=${API_KEY}&q=error&limit=1`)
+            console.log("Error: ", `${keyword} not found`)  
+        }
+    }
+
+
+    useEffect(() => {
+        if(keyword) fetchGifs()
+    }, [keyword]);
+
+
+    return gifUrl
+}
+
+export default useFetch;
