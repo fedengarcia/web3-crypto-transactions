4a4726cc2b5a884cfe7e2e86961947ca2ae4a94c
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-01-08 17:24:00 +0100

    Update useFetch.js
    Fix fetch url

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,14 @@
 import React, { useEffect,useState } from "react";
 
-const API_KEY =  process.env.GIPHY_API
+const API_KEY =  process.env.REACT_APP_GIPHY_API
 
 
 const useFetch = ({keyword}) => {
     const [gifUrl, setGifUrl] = useState("");
 
-
     const fetchGifs = async () => {
         try {
-            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`)
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`)
             const { data } = await response.json();
             setGifUrl(data[0]?.images?.downsized_medium?.url)
         } catch (error) {
